4372e4982fc55503a64617ab65fdba7242cdb403
Georginio397 <vinst.nftpromoter@gmail.com>
2025-12-22 03:32:47 +0200

    fix deps

--- server.js
+++ server.js
@@ -188,7 +188,7 @@ app.get("/api/leaderboard", async (req, res) => {
 // START
 // =================================================
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on 0.0.0.0:${PORT}`);
